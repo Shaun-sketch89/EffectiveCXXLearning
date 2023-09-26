31f36d8ab2d8116b1d888a87175f2e67cd90682a
yaoxuan <yaoxuan@airdoc.com>
2023-09-26 17:25:47 +0800

    Item14: Think carefully about copying behaviour in resource-managing classes. Stategy1: Prohibit copying

---
+++ Item14/s1.cpp
@@ -0,0 +1,48 @@
+//
+// Created by 姚轩 on 2023/9/26.
+//
+#include <iostream>
+
+// a toy Mutex class whose lock() and unlock() only print some infomation
+class Mutex{
+public:
+    void lock(){
+        std::cout << "lock Mutex" << std::endl;
+    }
+    void unlock(){
+        std::cout << "unlock Mutex" << std::endl;
+    }
+};
+
+// core of this strategy
+// in this scnerio, we do not allow copy of Lock, we define copy constructor and copy assignment constructor private,
+// to prevent Lock to be copyed where the program won't compile
+class Uncopyable{
+public:
+    Uncopyable(){};
+private:
+    Uncopyable(const Uncopyable& rhs);
+    Uncopyable& operator=(const Uncopyable& rhs);
+};
+
+// Lock used to manage lock and unlock operation of Mutex
+class Lock: public Uncopyable{
+public:
+    explicit Lock(Mutex *pm)
+    : Uncopyable(), mutexPtr(pm)
+    {mutexPtr->lock();}
+    ~Lock(){mutexPtr->unlock();}
+private:
+    Mutex *mutexPtr;
+};
+
+int main(){
+    Mutex m;
+    {
+        Lock ml1(&m);
+        // where client do this, program won't compile
+        Lock ml2(ml1);
+    }
+    std::cout << "out of block where Lock exists" << std::endl;
+    return 0;
+}
