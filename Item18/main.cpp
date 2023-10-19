c2d395205c319f0ebf1766e505d427cc28ca87de
yaoxuan <yaoxuan@airdoc.com>
2023-10-19 11:18:24 +0800

    define overloaded multiply function as non-member function

--- Item18/main.cpp
+++ Item18/main.cpp
@@ -25,16 +25,33 @@ public:
     // const-qualifying the return type from operator* can prevent clients from making error for user-defined types:
     // if(a * b = c)
     // we have our types behave consistently with the build-in types
+    // following defined as member function
+    /*
     const WrapperInt operator*(const WrapperInt& rhs){
         this->val *= rhs.val;
         return *this;
     }
-    int getVal(){
+     */
+
+    // note here we define member function const
+    // otherwise member function cannot be called by const object
+    // member function declared const cannot change the value of class
+    int getVal() const{
         return val;
+    }
+    void setVal(int v){
+        val = v;
     }
 private:
     int val;
 };
+
+//operator * normally defined as non-member function
+const WrapperInt operator*(const WrapperInt& lhs, const WrapperInt& rhs){
+    WrapperInt res(lhs);
+    res.setVal(lhs.getVal() * rhs.getVal());
+    return res;
+}
 
 int main(){
     Week today;
@@ -46,7 +63,7 @@ int main(){
     WrapperInt a(2);
     WrapperInt b(3);
     WrapperInt c;
-    // if *operator member function return const type, following won't compeile
+    // if *operator member function return const type, following won't compile
     //a * b = c;
     std::cout << c.getVal() << std::endl;
     c = a * b;
