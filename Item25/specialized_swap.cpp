c2d6f1f685e8d722d4dc2331c4ac5b42adf07735
yaoxuan <yaoxuan@airdoc.com>
2023-11-14 19:06:18 +0800

    Item25: Consider support for a non-throwing swap.

--- Item25/specialized_swap.cpp
+++ Item25/specialized_swap.cpp
@@ -1,12 +1,15 @@
 //
 // Created by 姚轩 on 2023/11/10.
 //
+#include <iostream>
 #include <vector>
 #include "widget.h"
 
+// specialize swap in namespace std
 namespace std{
     template<>
     void swap<Widget>(Widget& a, Widget& b){
+        std::cout << "specialized swap call" << std::endl;
         a.swap(b);
     }
 }
@@ -20,6 +23,11 @@ int main(){
     Widget widget_b(widget_impl_b);
 
     std::swap(widget_a, widget_b);
+
+    int aa = 1;
+    int bb = 3;
+    std::swap(aa, bb);
+    std::cout << aa << "; " << bb << std::endl;
 
     return 0;
 }
