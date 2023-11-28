5a8e9fcdb6f8238c11d3e2bf14a05648fac6bdc9
yaoxuan <yaoxuan@airdoc.com>
2023-11-28 18:49:05 +0800

    增加一些注释

--- Item28/main.cpp
+++ Item28/main.cpp
@@ -36,6 +36,7 @@ class Rectangle{
 public:
     Rectangle(const Point& ulhc, const Point& lrhc): pData(new RectData(ulhc, lrhc)){}
     // notice return value is not const, because ulhc and lrhc lie outside of Rectangle object!!!
+    // pData is const, but data pointed by pData is not const!!!
     Point& upperLeft() const {return pData->ulhc;}
     Point& lowerRight() const {return pData->lrhc;}
 private:
