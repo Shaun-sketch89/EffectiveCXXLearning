3d7c4d305c2098a45039ae6f123c8586a031d539
yaoxuan <yaoxuan@airdoc.com>
2024-01-10 14:27:40 +0800

    add a print function

--- object_oriented/main.cpp
+++ object_oriented/main.cpp
@@ -4,7 +4,13 @@
 #include <iostream>
 #include "bulk_item.h"
 
+void print_total(const Item_base& item, std::size_t n){
+    std::cout << "ISBN: " << item.book()
+              << "\tnumber sold: " << n << "\ttotal_price: "
+              << item.net_price(n) << std::endl;
+}
+
 int main() {
     Bulk_item bulkItem("0-201-54848-8", 32, 5, 0.9);
-    std::cout << bulkItem.net_price(10) << std::endl;
+    print_total(bulkItem, 10);
 }
