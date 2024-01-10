5ec83f50377dd12f2256a123809ddd24f5481826
yaoxuan <yaoxuan@airdoc.com>
2024-01-10 14:53:42 +0800

    modify derived class's function

--- object_oriented/book/bulk_item.cpp
+++ object_oriented/book/bulk_item.cpp
@@ -6,6 +6,6 @@
 Bulk_item::Bulk_item(const std::string &book, double sales_price, std::size_t min_qauntity, double discount_rate):
 Item_base(book, sales_price), min_qty(min_qauntity), discount(discount_rate) {}
 
-double Bulk_item::net_price(std::size_t n) const {return n < min_qty ? n * price : min_qty * price + (n - min_qty) * price * discount; }
+double Bulk_item::net_price(std::size_t n) const {return n < min_qty ? n * price : n * price * discount; }
 
 Bulk_item::~Bulk_item(){}
