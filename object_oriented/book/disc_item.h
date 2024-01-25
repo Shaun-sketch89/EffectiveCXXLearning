994a857a717f9ad1a01ac5c91209c8f76dbdbdee
yaoxuan <yaoxuan@airdoc.com>
2024-01-25 19:05:00 +0800

    define a pure virtual function in Disc_item class

--- object_oriented/book/disc_item.h
+++ object_oriented/book/disc_item.h
@@ -11,6 +11,9 @@ public:
               std::size_t qty = 0,
               double disc_rate = 0.0);
 
+    // define a pure virtual function to prevent user from creating a Disc_item object. This class only aims to be inherited.
+    virtual double net_price(std::size_t) const = 0;
+
     std::pair<std::size_t, double> discount_policy() const;
 
 protected:
