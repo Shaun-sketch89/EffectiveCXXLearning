43714552ca669179972d001d882a727655c83009
yaoxuan <yaoxuan@airdoc.com>
2024-02-28 17:06:32 +0800

    Item11: handle assignment to self in operator=.

---
+++ Item11/main.cpp
@@ -0,0 +1,23 @@
+//
+// AlignedArray holds a consecutive memory space.
+// Here we use it as an example class to implement the assignment operator introduced in item11 of C++ Effective
+// Deep copy is done in assignment operator and two alternatives are implemented to strive for both self-assignment-safe and exception-safe code!!
+// Created by 姚轩 on 2024/2/28.
+//
+#define ALIGNMENT 256
+typedef float scalar_t;
+const size_t ELEM_SIZE = sizeof(scalar_t);
+
+class AlignedArray {
+public:
+    AlignedArray(const size_t size) {
+        int ret = posix_memalign((void**)&ptr, ALIGNMENT, size * ELEM_SIZE);
+        if (ret != 0) throw std::bad_alloc();
+        this->size = size;
+    }
+    ~AlignedArray() { free(ptr); }
+    size_t ptr_as_int() {return (size_t)ptr; }
+private:
+    scalar_t* ptr;
+    size_t size;
+};
