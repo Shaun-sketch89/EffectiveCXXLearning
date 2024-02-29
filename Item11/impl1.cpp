3114ef0514c20a1967275c433f7c504878590612
yaoxuan <yaoxuan@airdoc.com>
2024-02-29 11:16:15 +0800

    a careful ordering of statements can yeild exception-safe (and self-assignment-safe) code

--- Item11/impl1.cpp
+++ Item11/impl1.cpp
@@ -4,6 +4,8 @@
 // Deep copy is done in assignment operator and two alternatives are implemented to strive for both self-assignment-safe and exception-safe code!!
 // Created by 姚轩 on 2024/2/28.
 //
+#include <iostream>
+
 #define ALIGNMENT 256
 typedef float scalar_t;
 const size_t ELEM_SIZE = sizeof(scalar_t);
@@ -17,15 +19,27 @@ public:
     }
     ~AlignedArray() { free(ptr); }
     AlignedArray& operator=(const AlignedArray& rhs){
+        // for debug
+        std::cout << "assignment operator called" << std::endl;
         scalar_t* pOrig = ptr;
         int ret = posix_memalign((void**)&ptr, ALIGNMENT, rhs.size * ELEM_SIZE);
         if (ret != 0) throw std::bad_alloc();
-        std::memcpy(ptr, rhs.ptr, rhs.size * ELEM_SIZE);
+        memcpy(ptr, rhs.ptr, rhs.size * ELEM_SIZE);
+        size = rhs.size;
         free(pOrig);
         return *this;
     }
     size_t ptr_as_int() {return (size_t)ptr; }
+    size_t size_of_arr() {return size;}
 private:
     scalar_t* ptr;
     size_t size;
 };
+
+int main(){
+    AlignedArray arr(50);
+    std::cout << arr.ptr_as_int() << " size: " << arr.size_of_arr() << std::endl;
+    AlignedArray arr2(100);
+    arr = arr2;
+    std::cout << arr.ptr_as_int() << " size: " << arr.size_of_arr() << std::endl;
+}
