6dfcce7251b0da7af5c5dfefa79516d80958fc29
yaoxuan <yaoxuan@airdoc.com>
2024-03-28 14:51:55 +0800

    The Template Method Pattern via the Non-Virtual Interface Idiom

---
+++ Item35/NVI.cpp
@@ -0,0 +1,37 @@
+//
+// Created by 姚轩 on 2024/3/28.
+//
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class GameCharacter{
+public:
+    // non-virtual interface(NVI) which is a more general design pattern called Template Method
+    int healthValue() const{
+        cout << "do before stuff\n";
+        int retVal = doHealthValue();
+        cout << "do after stuff\n";
+        return retVal;
+    }
+private:
+    // put virtual function to private
+    virtual int doHealthValue() const{
+        cout << "Game Character" << endl;
+        return 0;
+    }
+};
+
+class GameCharacterA: public GameCharacter{
+private:
+    // it's not strictly necessary that the virtual functions be private
+    virtual int doHealthValue() const{
+        cout << "Game Character A" << endl;
+        return 0;
+    }
+};
+
+int main(){
+    GameCharacter *gptr = new GameCharacterA();
+    gptr -> healthValue();
+}
