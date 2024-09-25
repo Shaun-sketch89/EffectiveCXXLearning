4cf1b8a236b22428cfcf365a0e71f67f0932148c
yaoxuan <yaoxuan@airdoc.com>
2024-09-25 14:39:25 +0800

    cursor code review

--- template/non-type-template-param.cpp
+++ template/non-type-template-param.cpp
@@ -9,9 +9,17 @@ template<typename T, std::size_t N> void array_init(T (&parm)[N]){
     }
 }
 
-template<typename T, std::size_t N> void array_display(const T* parm){
-    for(std::size_t i = 0; i != N; ++i){
-        std::cout << parm[i] << std::endl;
+/*
+模板形参说明:
+- T: 类型参数,表示数组元素的类型
+- N: 非类型模板参数,表示数组的大小
+
+函数形参说明:
+- T (&parm)[N]: 引用参数,引用一个类型为T、大小为N的数组
+*/
+template<typename T, std::size_t N> void array_display(const T (&parm)[N]) {
+    for(std::size_t i = 0; i < N; ++i) {
+        std::cout << parm[i] << ' ';
     }
 }
 
@@ -22,7 +30,8 @@ int main(){
     array_init(a);
     array_init(b);
 
-    array_display<int, 10>(a);
+    array_display(a);  // 不需要显式指定模板参数
+    array_display(b);
 
     return 0;
 }
