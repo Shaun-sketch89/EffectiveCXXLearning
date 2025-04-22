641977636977189397d1691418b9c41e7e27f0e5
shaun-matrix <yx20081989@126.com>
2025-04-22 19:59:58 +0800

    update test main

--- generic_handle/sales.cpp
+++ generic_handle/sales.cpp
@@ -7,7 +7,7 @@ int main(){
     for(std::size_t i = 0; i != 10; ++i){
         basket.add_item(bulk);
     }
-    Bulk_item bulk2("5-501-82470-1", 50, 5, 0.85);
+    Bulk_item bulk2("5-501-82470-1", 80, 5, 0.75);
     for(std::size_t i = 0; i != 5; ++i){
         basket.add_item(bulk2);
     }
