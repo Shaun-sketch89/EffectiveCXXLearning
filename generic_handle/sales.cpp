3175a80ee2a657093ba6397f5266c401311a821e
shaun-matrix <yx20081989@126.com>
2025-04-22 20:02:05 +0800

    update test main

--- generic_handle/sales.cpp
+++ generic_handle/sales.cpp
@@ -3,15 +3,23 @@
 
 int main(){
     Basket basket;
+	// 参与折扣的书籍1
     Bulk_item bulk("0-201-82470-1", 50, 5, 0.85);
+	// 买了10本
     for(std::size_t i = 0; i != 10; ++i){
         basket.add_item(bulk);
     }
+
+	// 参与折扣的书籍2
     Bulk_item bulk2("5-501-82470-1", 80, 5, 0.75);
+	// 买了5本
     for(std::size_t i = 0; i != 5; ++i){
         basket.add_item(bulk2);
     }
+
+	// 不参与折扣的书籍1
     Item_base bulk3("10-501-82470-1", 100);
+	// 买了10本
     for (std::size_t i = 0; i != 10; ++i) {
         basket.add_item(bulk3);
     }
